0dea438bb7df3105f1164ab89f6d04537f1050e6
Credilar Central <entregascredlar@gmail.com>
2022-05-23 17:01:15 -0300

    ajeitando os arquivos nas pastas

---
+++ rascunhos.py
@@ -0,0 +1,11 @@
+# vou utilizar aqui para fazer rascunhos como ja spoilei pelo nome do arquivo né hehehe
+# im gonna use this file just as sketch(dont know a better word than sketch. thx google translate:) )
+
+def func():
+    x = 'helloi munlrd'
+    print(x)
+
+x = 'teste'
+
+func()
+print(x)

486f30964e3353f860ad44315338150c2b3a7eb5
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2022-05-22 21:26:09 -0300

    Apenas alterei a ordem da inserção e remoção dos itens para ficar mais didatico e perceptivel.

--- Python/Estacio/exercicioLIFO.py
+++ Python/Estacio/exercicioLIFO.py
@@ -6,7 +6,7 @@ from time import sleep
 lifo = [] # the array that will be used for the exercise
 
 for count in range(10): #making a counting loop
-    lifo.append(count) # adding something to the array. I took advantage from the counting to do so
+    lifo.insert(0, count) # adding something to the array. I took advantage from the counting to do so
     print(lifo) #printing the array
     sleep(0.5) #taking a breath xD
 
@@ -16,7 +16,7 @@ sleep(2)
 
 for count in range(9, 0, -1): #Making a reverse counting loop
     print(lifo) #here i had to print before removing anything
-    lifo.pop(count) #removing the iten. also took advantage from the counting loop to do so
+    lifo.pop(0) #removing the iten form the array
     sleep(0.5)
 
-print(lifo) #had to show one last time because the for looping checks the parameters before executing the code. I could also use the do while loop, but im at the crazy life style. lol
+print(lifo) #had to show one last time because the for looping checks the parameters before executing the code. I could also use the "do while" loop, but im at the crazy life style. lol

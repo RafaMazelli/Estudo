0d26fc8aaaf8234d2338e812b182693f52f0f316
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2022-05-25 22:13:42 -0300

    apenas atualizando o arquivo rascunho após ter usado ele algumas vezes

--- rascunhos.py
+++ rascunhos.py
@@ -1,12 +1,9 @@
 # vou utilizar aqui para fazer rascunhos como ja spoilei pelo nome do arquivo né hehehe
 # im gonna use this file just as sketch(dont know a better word than sketch. thx google translate:) )
 
-x = 1
-def funcao():
-    global x
-    print('x = ',x)
-    x = 2
-    print('x global =',x)
+import datetime
+import calendar
 
-funcao()
-print('Valor de x=',x)
+agora = datetime.datetime.now()
+ano = agora.strftime('%Y')
+print(calendar.calendar(int(ano)))

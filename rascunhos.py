8c14337c058061601ce059b6ee079e0b7b6b5137
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2022-07-22 18:27:00 -0300

    treinando algoritmos de ordenação

--- rascunhos.py
+++ rascunhos.py
@@ -1,19 +1,21 @@
 # vou utilizar aqui para fazer rascunhos como ja spoilei pelo nome do arquivo né hehehe
 # im gonna use this file just as sketch (dont know a better word than sketch. thx google translate:) )
 
-teste = [43,2,5,44,333,6,55,44,343,23]
+teste = [43, 2, 5, 44, 333, 6, 55, 44, 343, 23]
 
-def bubble_sort(lista):
-    tamanho_lista = len(lista)
-    for i in range(tamanho_lista - 1):
-        for j in range(tamanho_lista-1):
-            if lista[j] > lista[j + 1]:
-                lista[j], lista[j+1] = lista[j+1], lista[j]
+
+def insertion_sort(lista):
+    t = len(lista)
+    for i in range(t):
+        item_analisado = lista[i]
+        indice_ordenados = i - 1
+        while indice_ordenados >= 0 and lista[indice_ordenados] > item_analisado:
+            lista[indice_ordenados + 1] = lista[indice_ordenados]
+            indice_ordenados -= 1
+        lista[indice_ordenados + 1] = item_analisado
 
 
 print(teste)
 print('-' * 50)
-bubble_sort(teste)
+insertion_sort(teste)
 print(teste)
-
-

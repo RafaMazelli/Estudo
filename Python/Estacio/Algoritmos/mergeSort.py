7b6dda492f91610ba258222ded1d54975d43daab
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2022-07-25 20:31:48 -0300

    nada aqui

--- Python/Estacio/Algoritmos/mergeSort.py
+++ Python/Estacio/Algoritmos/mergeSort.py
@@ -1,6 +1,3 @@
-import random
-
-
 
 def merge_sort(lista, inicio=0, fim=None):
     if fim is None:
@@ -34,20 +31,3 @@ def merge(lista, inicio, meio, fim):
 
 
 
-#-=-=-=-=-=-=-=TESTANDO O ALGORITMO=-=-=-=-=-=-=-
-#-=-=-=-=-=-=-=TESTING THE ALGORITHM=-=-=-=-=-=-=-
-array_de_numeros_aleatorios = random.sample(range(1,2000), 15)
-
-ordenados = [1,2,3,4,5,6,7,8,9]
-
-invertidos = [15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
-
-repetidos = [3,2,1,1,5,4,6,2,3,8,6,5,8,6,8]
-
-lista = repetidos
-print('=-' * 25)
-print(f'\33[1;31mLista >> {lista}\33[m')
-print('v-' * 25)
-merge_sort(lista)
-print(f'\33[1;32mLista Ordenada >> {lista}\33[m')
-print('=-' * 25)
--- rascunhos.py
+++ rascunhos.py
@@ -1,21 +1,24 @@
 # vou utilizar aqui para fazer rascunhos como ja spoilei pelo nome do arquivo né hehehe
 # im gonna use this file just as sketch (dont know a better word than sketch. thx google translate:) )
 
+
+
 teste = [43, 2, 5, 44, 333, 6, 55, 44, 343, 23]
 
 
-def insertion_sort(lista):
-    t = len(lista)
-    for i in range(t):
-        item_analisado = lista[i]
-        indice_ordenados = i - 1
-        while indice_ordenados >= 0 and lista[indice_ordenados] > item_analisado:
-            lista[indice_ordenados + 1] = lista[indice_ordenados]
-            indice_ordenados -= 1
-        lista[indice_ordenados + 1] = item_analisado
-
+def selection_sort(lista):
+    tamanho = len(lista)
+    for i in range(tamanho - 1):
+        index_min = i
+        for j in range(i, tamanho):
+            if lista[j] < lista[index_min]:
+                index_min = j
+        if lista[i] > lista[index_min]:
+            aux = lista[i]
+            lista[i] = lista[index_min]
+            lista[index_min] = aux
 
 print(teste)
 print('-' * 50)
-insertion_sort(teste)
+selection_sort(teste)
 print(teste)

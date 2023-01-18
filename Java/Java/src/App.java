faea9b6f140a5455925e89bacc93c1a3737f40cd
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-18 20:37:29 -0300

    praticando com java -estacio-

--- Java/Java/src/App.java
+++ Java/Java/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        //TODO Auto-generated method sub
+        Scanner sc = new Scanner(System.in);
+        double media, nota1, nota2;
+        System.out.println("Digite a nota 1:");
+        nota1 = Double.parseDouble(sc.nextLine());
+        System.out.println("Digite a 2 nota:");
+        nota2 = Double.parseDouble(sc.nextLine());
+        media = (nota1 + nota2) / 2.0;
+        System.out.println("A sua media eh:" + media);
+        sc.close();
     }
 }

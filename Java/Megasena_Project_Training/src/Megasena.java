5e154efec50d062ab2385725a03b7d0c7d5c6a60
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-21 19:49:40 -0300

    Atualizando o Projeto Megasena para ser interativo pela linha de comando -Estacio-

--- Java/Megasena_Project_Training/src/Megasena.java
+++ Java/Megasena_Project_Training/src/Megasena.java
@@ -1,13 +1,23 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Megasena {
     public static void main(String[] args) {
-        
-        Random generate = new Random();
 
-        for(int i = 0; i < 6; i++){
-            int number = generate.nextInt(60);
-            System.out.println(number);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Este programa irá gerar 6 numeros para o sorteio da MegaSena. Digite 1-Gerar // 2-Sair.");
+        String opcao = scanner.nextLine();
+
+        if(opcao.equals("1")){
+            Random generate = new Random();
+            for(int i = 0; i < 6; i++){
+                int number = generate.nextInt(60);
+                System.out.println(number);
+            }
+        }else{
+            System.out.println("Programa finalizado!");
         }
+
     }
 }

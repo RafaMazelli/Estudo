ab3e672e5bd07d09f4bfb64883ef508138e9e9b3
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-21 17:12:28 -0300

    Finalização do programa de calculadora simples em java -Estacio-

--- Java/Calculator_Project_training/src/Calculator.java
+++ Java/Calculator_Project_training/src/Calculator.java
@@ -1,5 +1,35 @@
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        if(args[0].equals("somar")){
+            sum(x, y);
+        }else if(args[0].equals("subtrair")){
+            minus(x, y);
+        }else if(args[0].equals("multiplicar")){
+            plus(x, y);
+        }else if(args[0].equals("dividir")){
+            divide(x, y);
+        }else{
+            System.out.println("Não existe a instrução: " + args[0]);
+        }
     }
+
+    static void sum(int x, int y){
+        System.out.println(x + y);
+    }
+
+    static void minus(int x, int y){
+        System.out.println(x - y);
+    }
+    
+    static void plus(int x, int y){
+        System.out.println(x * y);
+    }
+
+    static void divide(int x, int y){
+        System.out.println(x / y);
+    }
+
 }

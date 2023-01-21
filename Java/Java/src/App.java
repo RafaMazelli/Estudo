b342f234a62ef79e89b5b3121743cb53f0cd625c
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-21 16:23:25 -0300

    Treinando fundamentos java -Estacio-

--- Java/Java/src/App.java
+++ Java/Java/src/App.java
@@ -2,23 +2,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        
-        sum(100, 2);
-        
-    }
-
-
-    static void sum(int x, int y){
-       
-        System.out.print("soma:");
-        System.out.println(x + y);
-        System.out.print("Subtração:");
-        System.out.println(x - y);
-        System.out.print("Multiplicação:");
-        System.out.println(x * y);
-        System.out.print("Divisão:");
-        System.out.println(x / y);
-
+    
+        System.out.println(args[0]);
 
     }
+
 }

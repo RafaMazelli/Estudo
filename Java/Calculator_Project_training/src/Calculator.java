a25006bca8fc137b93bc24bba4ba409f511d4094
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-21 18:33:12 -0300

    Deixando o projeto, Calculator em Java, interativo na linha de comando utilizando Scanner -Estacio-

--- Java/Calculator_Project_training/src/Calculator.java
+++ Java/Calculator_Project_training/src/Calculator.java
@@ -1,35 +1,45 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        int x = Integer.parseInt(args[1]);
-        int y = Integer.parseInt(args[2]);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero:");
+        String numero1 = scanner.nextLine();
+        int x = Integer.parseInt(numero1);
+        System.out.println("Digite o segundo numero:");
+        String numero2 = scanner.nextLine();
+        int y = Integer.parseInt(numero2);
 
-        if(args[0].equals("somar")){
+        System.out.println("Digite: 1-Somar // 2-Subtrair // 3-Multiplicar // 4-Dividir:");
+        String operacao = scanner.nextLine();
+
+        if(operacao.equals("1")){
             sum(x, y);
-        }else if(args[0].equals("subtrair")){
+        }else if(operacao.equals("2")){
             minus(x, y);
-        }else if(args[0].equals("multiplicar")){
+        }else if(operacao.equals("3")){
             plus(x, y);
-        }else if(args[0].equals("dividir")){
+        }else if(operacao.equals("4")){
             divide(x, y);
         }else{
-            System.out.println("Não existe a instrução: " + args[0]);
+            System.out.println("Não existe a instrução: " + operacao);
         }
     }
 
     static void sum(int x, int y){
-        System.out.println(x + y);
+        System.out.println("O resultado foi: " + (x + y));
     }
 
     static void minus(int x, int y){
-        System.out.println(x - y);
+        System.out.println("O resultado foi: " + (x - y));
     }
     
     static void plus(int x, int y){
-        System.out.println(x * y);
+        System.out.println("O resultado foi: " + (x * y));
     }
 
     static void divide(int x, int y){
-        System.out.println(x / y);
+        System.out.println("O resultado foi: " + (x / y));
     }
 
 }

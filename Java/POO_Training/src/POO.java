f4723c4bc4e8c14db3f3d052c0e5ca2c483f8d27
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-22 13:14:30 -0300

    Adicionando metodos e comentarios a classe User do projeto de treino POO na linguagem java -Estacio-

--- Java/POO_Training/src/POO.java
+++ Java/POO_Training/src/POO.java
@@ -1,17 +1,25 @@
 public class POO {
     public static void main(String[] args) {
 
-        User usuario1 = new User();
-            usuario1.firstName = "Josefino";
-            usuario1.lastName = "Pequenino";
-            usuario1.password = 1432;
-        
+        User usuario1 = new User(); // instanciando objeto da classe User
+            usuario1.firstName = "Josefino"; // alterando propriedade do objeto instanciado
+            usuario1.lastName = "Pequenino";// alterando propriedade do objeto instanciado
+            usuario1.password = 1432;// alterando propriedade do objeto instanciado
+            
         User usuario2 = new User();
             usuario2.firstName = "Marialta";
             usuario2.lastName = "Grandosvalda";
             usuario2.password = 13131;
+            String fullName = usuario2.fullName();
 
-        System.out.println("Nome: " + usuario1.firstName + " " + usuario1.lastName + " // senha: " + usuario1.password);
+
+        System.out.println("Nome: " + usuario1.firstName + " " + usuario1.lastName + " // senha: " + usuario1.password + " // " + "logado: " + usuario1.isLogged);
         System.out.println("Nome: " + usuario2.firstName + " " + usuario2.lastName + " // senha: " + usuario2.password);
+    
+        usuario1.setLogged(true);
+
+        System.out.println("Nome: " + usuario1.firstName + " " + usuario1.lastName + " // senha: " + usuario1.password + " // " + usuario1.isLogged);
+        System.out.println("Nome: " + usuario2.firstName + " " + usuario2.lastName + " // senha: " + usuario2.password  + " // Nome Completo: " + fullName);
+    
     }
 }
--- Java/POO_Training/src/User.java
+++ Java/POO_Training/src/User.java
@@ -1,7 +1,16 @@
 public class User {
     
-    public String firstName;
-    public String lastName;
-    public int password;
+    public boolean isLogged; // definindo propriedade do molde do objeto
+    public String firstName; // definindo propriedade do molde do objeto
+    public String lastName; // definindo propriedade do molde do objeto
+    public int password; // definindo propriedade do molde do objeto
+
+    public void setLogged(boolean logged){ // definindo um metodo
+        isLogged = logged;
+    }
+
+    public String fullName(){ //definindo um metodo com retorno
+        return firstName + " " + lastName;
+    }
     
 }

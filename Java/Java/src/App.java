635a793dc135f892fecaec127798e9e38ffeee9b
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-22 12:17:46 -0300

    Testando formas de comparação de Strings -Estacio-

--- Java/Java/src/App.java
+++ Java/Java/src/App.java
@@ -1,10 +1,43 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args){
+    static String password = new String("123"); // senha num BD
+    static String senha = new String("123"); // senha predigitada
     
-        System.out.println(args[0]);
+    public static void main(String[] args){
+
+        
+        ComparandoStrings1(password, senha); // resultado esperado false
+        ComparandoStrings2(); // resultado esperado false
+        ComparandoStrings3(password, senha); // resultado esperado true
+    
+    }
+
+    static void ComparandoStrings1(String p, String s){
+
+
+            System.out.println(password == senha); //O resultado será falso pois está comparando 2 espacos diferentes na memoria(suas referencias)
+
+            System.out.println();
+        }
+
+    static void ComparandoStrings2(){
+
+            String password = "123"; // senha do usuario no banco
+            String senha = "123"; // senha predigitada
+            System.out.println(password == senha); // o == está apontando que as 2 variaveis comparadas estão apontando para o mesmo espaço de memoria. o resultado pode ser verdadeiro
+
+            System.out.println();
 
     }
 
-}
+    static void ComparandoStrings3(String p, String s){
+
+
+            System.out.println(password.equals(senha)); // está comparando se o conteúdo das variaveis são iguais
+
+            System.out.println();
+        }
+
+    }
+

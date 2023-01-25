34600fc415f75143ee6b68e8de12885544be6690
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-24 23:47:45 -0300

    Testing the arrays content comparison between 2 identical arrays (still in progress) -Estacio-

--- Java/Java/src/App.java
+++ Java/Java/src/App.java
@@ -1,43 +1,23 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class App {
-    static String password = new String("123"); // senha num BD
-    static String senha = new String("123"); // senha predigitada
-    
+    //public static int[] numbersA = new int[]{2, 5, 3, 4, 55, 76 ,4 ,1};
+    //public int[] numbersB = new int[]{2, 5, 3, 4, 55, 76 ,4 ,1};
     public static void main(String[] args){
-
+        int[] numbersA = new int[]{2, 5, 3, 4, 55, 76 ,4 ,1};
+        sortArray(numbersA);
         
-        ComparandoStrings1(password, senha); // resultado esperado false
-        ComparandoStrings2(); // resultado esperado true
-        ComparandoStrings3(password, senha); // resultado esperado true
-    
-    }
-
-    static void ComparandoStrings1(String p, String s){
-
-
-            System.out.println(password == senha); //O resultado será falso pois está comparando 2 espacos diferentes na memoria(suas referencias)
-
-            System.out.println();
-        }
-
-    static void ComparandoStrings2(){
-
-            String password = "123"; // senha do usuario no banco
-            String senha = "123"; // senha predigitada
-            System.out.println(password == senha); // o == está apontando que as 2 variaveis comparadas estão apontando para o mesmo espaço de memoria. o resultado pode ser verdadeiro
-
-            System.out.println();
 
     }
-
-    static void ComparandoStrings3(String p, String s){
-
-
-            System.out.println(password.equals(senha)); // está comparando se o conteúdo das variaveis são iguais
-
-            System.out.println();
-        }
-
+    public void comparingArray(int[] a, int[] b){
+        System.out.println(Arrays.equals(a, b));
     }
+
+    public int[] sortArray(int[] a){
+        Arrays.sort(a);
+        return a;
+    }
+}
+
+
 

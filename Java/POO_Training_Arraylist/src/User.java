26f0a018fe6ecf82b4a754b51083c97cb0837508
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-24 22:06:25 -0300

    Creation of a new test project in Java to test arraylists and also practice english a little bit on the comments -Estacio-

---
+++ Java/POO_Training_Arraylist/src/User.java
@@ -0,0 +1,50 @@
+public class User {
+    
+    private boolean isLogged; // definindo propriedade do molde do objeto
+    private String firstName; // definindo propriedade do molde do objeto
+    private String lastName; // definindo propriedade do molde do objeto
+    private int password; // definindo propriedade do molde do objeto
+
+    //----------------------CONSTRUCTOR----------------------------------
+
+    public User(boolean isLogged, String firstName, String lastName, int password ){ // Construtor da classe User
+
+        this.isLogged = isLogged;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    //-----------------------SETTERS------------------------------------
+    public void setLogged(boolean logged){ // definindo metodo setter
+        isLogged = logged;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setPassword(int password){
+        this.password = password;
+    }
+
+    //-----------------------GETTERS------------------------------------
+
+    public Boolean getLogged(){//definindo Getters. A substituição do void pelo tipo específico mostra que alem de um retorno a função tem um retorno especifico
+        return isLogged;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getPassword(){
+        return password;
+    }
+    public String getFullName(){ 
+        return firstName + " " + lastName;
+    }
+    
+}

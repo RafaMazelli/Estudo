f2946453e7890142c47a825449c7dc52edba071b
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-26 15:23:17 -0300

    Changing the iteration method using the forEach to print the objects. -Estacio-

--- Java/POO_Training_Arraylist/src/POO.java
+++ Java/POO_Training_Arraylist/src/POO.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+//So, english from now on.. just because i want to :).. well, this example project is to show how an arraylist actually works, and its property is that you can add things on it after his creation, and that is what i've done down bellow,, first i created the arraylist, then i instanciated the objects using the while loop. After that i chose one of the objects created and printed, then i instantiated another object (to test and prove the array property) and printed it. 
 
 public class POO {
     
     public static void main(String[] args){
     //----------------------CRIANDO ARRAYLIST-----------------------------
-    
-    //So, english from now on.. just because i want to :).. well, this example project is to show how an arraylist actually works, and its property is that you can add things on it after his creation, and that is what i've done down bellow,, first i created the arraylist, then i instanciated the objects using the while loop. After that i chose one of the objects created and printed, then i instantiated another object (to test and prove the array property) and printed it. 
 
     List<User> users = new ArrayList<>();
     
@@ -20,7 +19,12 @@ public class POO {
         i++;
     }
     //---------------------PRINTING AN OBJECT EXAMPLE---------------------
-    System.out.println(users.get(9).getFullName());
+    
+    for (User name : users){
+        System.out.println(name.getFullName());
+    }
+    
+    //System.out.println(users.get(9).getFullName());
 
     //-----------------INSTANTIATING ANOTHER OBJECT----------------------
     User user11 = new User(false,"sicrano " + i, "da silcrano " + i, 123);

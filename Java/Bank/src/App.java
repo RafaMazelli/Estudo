da84fb8303e1725cad10430f9fced327c4ac4100
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-27 00:02:15 -0300

    Creation of the structure of a simple bank project in java to test all the concepts learned so far. -Estacio-

---
+++ Java/Bank/src/App.java
@@ -0,0 +1,13 @@
+
+public class App {
+
+    //Just made a test trying to change the source path of the code on the settings.json file of this project.. The class and varible names is just for fun :)
+    
+    public static void main(String[] args) throws Exception {
+        
+        aiai uiui = new aiai();
+        uiui.aiaai();
+    }
+
+   
+}
---
+++ Java/Bank/src/aiai.java
@@ -0,0 +1,8 @@
+
+public class aiai{
+    private String aiai = "aiai!";
+
+    public void aiaai(){
+        System.out.println(aiai);
+    }
+}

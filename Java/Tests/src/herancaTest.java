127ab24e688cf7f972c54d2440b8262daaef3e7e
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-27 22:53:19 -0300

    Created a project just to make tests. -Estacio-

---
+++ Java/Tests/src/herancaTest.java
@@ -0,0 +1,15 @@
+public class herancaTest {
+
+    public class pessoa{
+        private String primeiroNome;
+        private String ultimoNome;
+        private int idade;
+        private String sexo;
+        
+    }
+
+    public class empregado extends pessoa{
+        private String profissao;
+    }
+    
+}

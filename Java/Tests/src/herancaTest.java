664b3c2fd87075f179b74ab913e7f2772c87234a
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-01-28 21:47:18 -0300

    changing a few things. -Estacio-

--- Java/Tests/src/herancaTest.java
+++ Java/Tests/src/herancaTest.java
@@ -1,15 +1,3 @@
 public class herancaTest {
-
-    public class pessoa{
-        private String primeiroNome;
-        private String ultimoNome;
-        private int idade;
-        private String sexo;
-        
-    }
-
-    public class empregado extends pessoa{
-        private String profissao;
-    }
     
 }

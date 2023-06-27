3938bd78dd25291d31278b387ebf3ebfd81691f9
RafaMazelli <53786787+RafaMazelli@users.noreply.github.com>
2023-06-26 23:13:06 -0300

    testando app aula estacio

--- react-native/teste-estacio/App.js
+++ react-native/teste-estacio/App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Vontade de cagar dos caraio!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import React from 'react';
+ import { View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+ 
+ const DADOS = [
+     {
+     id: '1',
+     descricao: 'TV Led 49',
+     categoria_id: 1
+     },
+     {
+     id: '4',
+     descricao: 'Camisa Trilha',
+     categoria_id: 2
+     },
+     {
+     id: '4',
+     descricao: 'Qualquer semelhança é mera coincidência',
+     categoria_id: 3
+     },
+ ];
+ 
+ const Item = ({ descricao }) => (
+     <View style={styles.item}>
+     <Text style={styles.title}>{descricao}</Text>
+     </View>
+ );
+ 
+ const App = () => {
+     const renderItem = ({ item }) => (
+     <Item descricao={item.descricao} />
+     );
+ 
+     return (
+     <View style={styles.container}>
+         <FlatList
+         data={DADOS}
+         renderItem={renderItem}
+         keyExtractor={item => item.id}
+         />
+     </View>
+     );
+ }
+ 
+ const styles = StyleSheet.create({
+     container: {
+     flex: 1,
+     marginTop: StatusBar.currentHeight || 0,
+     },
+     item: {
+     backgroundColor: 'yellow',
+     padding: 20,
+     marginVertical: 8,
+     marginHorizontal: 16,
+     },
+     title: {
+     fontSize: 12,
+     },
+ });
+ 
+ export default App; 
